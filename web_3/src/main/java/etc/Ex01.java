608d2a5f09b98ebcea302c2ff8da75acfaaa4985
DESKTOP-7KDHQKS\USER <0813bbb@naver.com>
2022-04-08 13:30:23 +0900

    Ex01.java -> sysout 10번을 for 반복문으로 수정

--- web_3/src/main/java/etc/Ex01.java
+++ web_3/src/main/java/etc/Ex01.java
@@ -5,16 +5,8 @@ public class Ex01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		for (int i = 1; i<= 10; i++ ) {
 		System.out.println("Hello world~!");
-		System.out.println("Hello world~!");
-		System.out.println("Hello world~!");
-		System.out.println("Hello world~!");
-		System.out.println("Hello world~!");
-		System.out.println("Hello world~!");
-		System.out.println("Hello world~!");
-		System.out.println("Hello world~!");
-		System.out.println("Hello world~!");
-		System.out.println("Hello world~!");
+		}
 	}
 }
