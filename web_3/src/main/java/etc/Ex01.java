110e04413df12e89a774f6fed4f614f2b0692565
DESKTOP-7KDHQKS\USER <0813bbb@naver.com>
2022-04-08 14:09:00 +0900

    Ex01.java -> while 문으로 반복하도록 수정

--- web_3/src/main/java/etc/Ex01.java
+++ web_3/src/main/java/etc/Ex01.java
@@ -5,8 +5,12 @@ public class Ex01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for (int i = 1; i<= 10; i++ ) {
-		System.out.println("Hello world~!");
+		int count = 1;
+		
+		while (count <= 10) {
+			
+			count ++; 
+			System.out.println("Hello world~!");
 		}
 	}
 }
